c23c070f3ff3dbed210b4e6e09aec43e487725e0
mahajanhrishikesh <mahajanhm4@gmail.com>
2020-05-22 12:08:43 +0530

    Final Commit

--- ard/forms.py
+++ ard/forms.py
@@ -22,3 +22,27 @@ class UpdateAccountForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	submit = SubmitField('Update')
 	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+
+class AddProductForm(FlaskForm):
+	asin = StringField('ASIN', validators=[DataRequired(), Length(min=2, max=20)])
+	name = StringField('Name', validators=[DataRequired(), Length(min=2, max=250)])
+	customer_rating = StringField('Customer Rating', validators=[DataRequired(), Length(min=1,max=20)])
+	price = StringField('Price', validators=[DataRequired(), Length(min=2, max=10)])
+	shipping = StringField('Shipping', validators=[DataRequired(), Length(min=2, max=10)])
+	sold_by = StringField('Sold By', validators=[DataRequired(), Length(min=2, max=15)])
+	color = StringField('Color', validators=[DataRequired(), Length(min=2, max = 15)])
+	submit = SubmitField('Add')
+
+class AddCommentForm(FlaskForm):
+	review_text = StringField('Review Text', validators=[DataRequired(), Length(min=4, max=300)])
+	submit = SubmitField('Add Comment')
+
+class DeleteForm(FlaskForm):
+	submit = SubmitField('Delete Product')
+
+class UpdateForm(FlaskForm):
+	fieldname = StringField('Field Name', validators=[DataRequired(), Length(min=4, max=100)])
+	fieldvalue = StringField('Field Value', validators=[DataRequired(), Length(min=4, max=100)])
+	submit = SubmitField('Update Product')
+
+
--- ard/routes.py
+++ ard/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, redirect, flash, request
 from ard import app, mongo, bcrypt, login_manager
-from ard.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from ard.forms import RegistrationForm, LoginForm, UpdateAccountForm, AddProductForm, AddCommentForm, DeleteForm, UpdateForm
 from flask_login import login_user, current_user, logout_user, login_required
 from ard.models import User
 from flask import session
@@ -18,7 +18,10 @@ def home():
 		print(session['username'])
 	prods = list(mongo.db.appliances.find().limit(30))
 	image_file = url_for('static', filename="images/sentiment_overall.png")
-	return render_template('home.html', title='Home', prods=prods, image_file=image_file)
+	image_file_2 = url_for('static', filename="images/wc.png")
+	csv_file = url_for('static', filename="csvs/Brand_wise_data.csv")
+	csv_file_2 = url_for('static', filename='csvs/Category_data.csv')
+	return render_template('home.html', title='Home', prods=prods, image_file=image_file, csv_file=csv_file, csv_file_2=csv_file_2, image_file_2=image_file_2)
 
 @app.route('/products')
 def products():
@@ -28,7 +31,8 @@ def products():
 @app.route("/products/<productID>")
 def product_profile(productID):
 	product = mongo.db.appliances.find_one_or_404({"asin": productID})
-	return render_template("product.html",product=product)
+	reviews = list(mongo.db.appliances_reviews.find({"asin":productID}, {"_id":0, "reviewText":1, "reviewerName":1}))
+	return render_template("product.html",product=product, reviews=reviews)
 
 @app.route('/about')
 def about():
@@ -86,4 +90,57 @@ def account():
 	elif request.method == 'GET':
 		form.username.data = session['username']
 		form.email.data = session['email']
-	return render_template('account.html', title='Account', form=form)
+	image_file = url_for('static', filename="images/default.jpg")
+	return render_template('account.html', title='Account', form=form, image_file=image_file)
+
+@app.route("/addProduct", methods=['GET', 'POST'])
+def addProduct():
+	form = AddProductForm()
+	product = {}
+	if form.validate_on_submit():
+		product['asin'] = form.asin.data
+		product['name'] = form.name.data
+		product['features'] = {"Customer Rating": form.customer_rating.data, "price": form.price.data, "Shipping": form.shipping.data, "Sold By": form.sold_by.data, "Color":form.color.data}
+		mongo.db.appliances.insert_one(product)
+		flash(f'Product Inserted into DB!', 'success')
+		return redirect(url_for('product_profile', productID = form.asin.data))
+	return render_template('addProduct.html', title='Add Product', form=form)
+
+@app.route("/network", methods=['GET', 'POST'])
+def network():
+	iframe = 'https://preview.flourish.studio/2502679/zZM-m91nGmVDVFia-tVA4G5kD0DpllwX_u_A-ObdKOrQOEtM47CVdK6HEU1QlYo9/'
+	return render_template('network.html', title='Network', iframe=iframe)
+
+@app.route("/comments/<productID>", methods=['GET', 'POST'])
+def addComment(productID):
+	form = AddCommentForm()
+	review = {}
+	if form.validate_on_submit():
+		review['reviewText'] = form.review_text.data
+		if session['is_active']:
+			review['reviewerName'] = session['username']
+		else:
+			review['reviewerName'] = 'Anonymous'
+		review['asin'] = productID
+		mongo.db.appliances_reviews.insert_one(review)
+		return redirect(url_for('product_profile', productID = productID))
+	return render_template('addComment.html', title="Add Commment", form=form)
+
+@app.route("/delete/<productID>", methods=['GET', 'POST'])
+def deleteProduct(productID):
+	form = DeleteForm()
+	if form.validate_on_submit():
+		mongo.db.appliances.delete_one({"asin":productID})
+		return redirect(url_for('products'))
+	return render_template('deleteProduct.html', title="Delete Product", form=form)
+
+@app.route("/update/<productID>", methods=['GET', 'POST'])
+def updateProduct(productID):
+	form = UpdateForm()
+	update={}
+	if form.validate_on_submit():
+		update[form.fieldname.data] = form.fieldvalue.data
+		mongo.db.appliances.update_one({"asin":productID}, {"$set": update})
+		print(update)
+		return redirect(url_for('product_profile', productID = productID))
+	return render_template('updateProduct.html', title="Update Product", form=form)
